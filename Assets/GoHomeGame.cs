655abf98ed1a7922617c4b937c8f06ccce1cb10f
Adam Goth <adamgoth@gmail.com>
2017-02-02 20:09:20 -0700

    Finishes game

---
+++ Assets/GoHomeGame.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoHomeGame : MonoBehaviour {
+
+	public Vector2 playerLocation;
+	public Vector2 homeLocation;
+	bool gameIsOver = false;
+
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Go Home!");
+		print ("A game where you need to find your way back.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!gameIsOver) {
+			UpdateMovement (KeyCode.LeftArrow, new Vector2 (-1, 0));
+			UpdateMovement (KeyCode.RightArrow, new Vector2 (1, 0));
+			UpdateMovement (KeyCode.UpArrow, new Vector2 (0, 1));
+			UpdateMovement (KeyCode.DownArrow, new Vector2 (0, -1));
+		}
+	}
+
+	private void UpdateMovement (KeyCode kc, Vector2 movementVector)
+	{
+		if (Input.GetKeyDown(kc)) {
+			playerLocation = playerLocation + movementVector;
+			PrintDistanceToHome ();
+		}
+	}
+
+	private void PrintDistanceToHome() 
+	{
+		Vector2 pathToHome = homeLocation - playerLocation;
+		print ("Distance to home: " + pathToHome.magnitude);
+		if (playerLocation == homeLocation) {
+			print ("I am home!");
+			gameIsOver = true;
+		}
+	}
+}
